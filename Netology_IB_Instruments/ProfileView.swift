f32d08a593b496dc36d2e6c2dee331c34490e014
Sergey Yarkevich <senja006@users.noreply.github.com>
2021-11-24 11:30:34 +0300

    homework done

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Яркевич Сергей on 24.11.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var city: UILabel!
+}
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Яркевич Сергей on 24.11.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+
+}
